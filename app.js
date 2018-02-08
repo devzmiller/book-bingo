df6b8766dc5a621be1075369afcf5b268c5c5136
Devin Miller <devzmiller@gmail.com>
2018-02-08 15:04:52 -0800

    Set up to post a card from json to the database and return as json

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
-const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config();
+const MongoClient = require('mongodb').MongoClient;
 
-MongoClient.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ds053320.mlab.com:53320/book-bingo`, (err, database) => {
+let db;
 
+MongoClient.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ds053320.mlab.com:53320/book-bingo`, (err, client) => {
+  if (err) return console.log(err);
+  db = client.db('book-bingo');
 });
 
 const app = express()
@@ -15,8 +18,23 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+app.post('/cards', (req, res) => {
+  console.log(req.body.title)
+  db.collection('cards').save({
+    "title": req.body.title,
+    "creator_id": req.body.creator_id,
+    "categories": req.body.categories
+  })
+  db.collection('cards').findOne({
+    $and: [
+      {"title": req.body.title}, {"creator_id": req.body.creator_id}
+    ]
+  }).then((card) => res.send(card));
+})
+
 app.get('/', (req, res) => res.status(200).send({
   message: 'Hello World!',
 }));
+
 
 module.exports = app;

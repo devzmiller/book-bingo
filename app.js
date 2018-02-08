740159449a7a32f1496ce2abc75eb251ea9a7432
Devin Miller <devzmiller@gmail.com>
2018-02-08 15:55:46 -0800

    Write index and view cards routes, refactor post cards route, create post books route.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const logger = require('morgan');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectId;
 
 let db;
 
@@ -19,17 +20,28 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.post('/cards', (req, res) => {
-  console.log(req.body.title)
-  db.collection('cards').save({
+  db.collection('cards').insertOne({
     "title": req.body.title,
     "creator_id": req.body.creator_id,
     "categories": req.body.categories
-  })
-  db.collection('cards').findOne({
-    $and: [
-      {"title": req.body.title}, {"creator_id": req.body.creator_id}
-    ]
-  }).then((card) => res.send(card));
+  }).then((cards) => res.send(cards.ops[0]))
+})
+
+app.get('/cards', (req, res) => {
+  db.collection('cards').find().toArray((err, cards) => res.send(cards))
+})
+
+app.get('/cards/:cardId', (req, res) => {
+  db.collection('cards').findOne({"_id": ObjectId(req.params.cardId)}).then((card) => res.send(card));
+})
+
+app.post('/cards/:cardId/books', (req, res) => {
+  db.collection('books').insertOne({
+    "card_id": ObjectId(req.params.cardId),
+    "category": req.body.category,
+    "title": req.body.title
+  }).then((books) => res.send(books.ops[0]))
+
 })
 
 app.get('/', (req, res) => res.status(200).send({

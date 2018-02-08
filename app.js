c5266a36251a99ffaa0e43d3d74851b804f31c4c
Devin Miller <devzmiller@gmail.com>
2018-02-08 13:50:10 -0800

    Add dotenv and set up db connection.

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config();
+
+MongoClient.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ds053320.mlab.com:53320/book-bingo`, (err, database) => {
+
+});
 
 const app = express()
 
